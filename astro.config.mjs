84a0887d471ebd2274110764fa9a7a8d48e037bd
saidMounaim <saidmounaim00@gmail.com>
2024-01-23 09:56:15 +0100

    Istall dotenv & Fecth Movies API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import dotenv from 'dotenv'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+
+dotenv.config()
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	integrations: [tailwind()]
+})
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,6 @@
-export const API_URL = "https://api.themoviedb.org/3/movie/";
+export const API_URL = 'https://api.themoviedb.org/3/movie/'
+
+export async function getMovies(type: string = 'popular') {
+	const res = await fetch(`${API_URL}${type}?api_key=${process.env.API_KEY}&language=en-US`)
+	return res.json()
+}
